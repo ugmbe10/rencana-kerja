fcae7d7d7e78d6c4a9fcd435a368d299a3f4d843
maratus_sholekhah <maratuss12@gmail.com>
2020-10-25 15:44:12 +0700

    update and delete features has changed

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -3,7 +3,6 @@ import Task from '../models/task.js';
 import jwt from 'jsonwebtoken';
 import bodyParser from 'body-parser';
 import Conf from './../utils/config.js';
-import Status from './../utils/status.js';
 
 const taskRouter = express.Router();
 
@@ -29,14 +28,9 @@ taskRouter.put('/update-status/:id', async(req, res) =>{
             return res.status(500).send({ 
                 auth: false, 
                 message: 'Failed to authenticate token.' });
-        const username = decoded.user.username;
 
         if(task){
-            if(task.status === Status[0]){
-                task.assigned_to = username;
-            } 
             task.status = status;
-
             const updatedTask = await task.save();
             res.status(200).send(updatedTask);
         }
@@ -50,21 +44,38 @@ taskRouter.put('/update-status/:id', async(req, res) =>{
 taskRouter.delete('/delete/:id', async(req, res) => {
     const task = await Task.findById(req.params.id);
 
-    if(task){
-        if(!task.assigned_to || task.assigned_to===null || task.assigned_to===""){
-            await task.remove();
-            res.json({
-                message: 'Task terhapus'
+    //mengambil token dari header
+    var token = req.headers['x-access-token'];
+    if (!token) 
+        return res.status(401).send({ 
+            auth: false, 
+            message: 'No token provided.' 
+        });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, decoded) => {
+        if (err) 
+            return res.status(500).send({ 
+                auth: false, 
+                message: 'Failed to authenticate token.' });
+
+        if(task){
+            if(!task.assigned_to){
+                res.json({
+                    message: 'Task tidak bisa dihapus'
+                });
+            } else {
+                await task.remove();
+                res.json({
+                    message: 'Task terhapus'
+                });
+            }
+        } else {
+            res.status(404).json({
+                message: 'Task tidak ditemukan'
             });
         }
-        res.json({
-            message: 'Task tidak bisa dihapus'
-        });
-    } else {
-        res.status(404).json({
-            message: 'Task tidak ditemukan'
-        });
-    };
-});
+    })
+})
 
 export default taskRouter;
